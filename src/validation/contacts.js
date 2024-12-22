456a309cdabdc5b23c2caef64971655549e11625
ogoncharenko32 <krapniknil32@gmail.com>
2024-12-22 18:12:06 +0200

    hw-04 done

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,11 +1,12 @@
 import Joi from 'joi';
+import { contactTypeList } from '../constants/contacts.js';
 
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.string().min(3).max(20).required(),
   email: Joi.string().min(3).max(20).required(),
   isFavorite: Joi.boolean(),
-  contactType: Joi.string().valid('work', 'home', 'personal'),
+  contactType: Joi.string().valid(...contactTypeList),
 });
 
 export const updateContactSchema = Joi.object({
@@ -13,5 +14,5 @@ export const updateContactSchema = Joi.object({
   phoneNumber: Joi.string().min(3).max(20),
   email: Joi.string().min(3).max(20),
   isFavorite: Joi.boolean(),
-  contactType: Joi.string().valid('work', 'home', 'personal'),
+  contactType: Joi.string().valid(...contactTypeList),
 });
