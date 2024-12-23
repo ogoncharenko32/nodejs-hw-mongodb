9aa3953a20c89aafc12f777b2cdecc9fda52cf04
ogoncharenko32 <krapniknil32@gmail.com>
2024-12-23 15:43:09 +0200

    fix: email required removed

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -4,7 +4,7 @@ import { contactTypeList } from '../constants/contacts.js';
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.string().min(3).max(20).required(),
-  email: Joi.string().min(3).max(20).required(),
+  email: Joi.string().min(3).max(20),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid(...contactTypeList),
 });
